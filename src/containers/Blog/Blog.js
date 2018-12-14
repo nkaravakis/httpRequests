d0ab78c9a92eb34ab602c2dbaf51ce93af4d9652
Nick Karavakis <nkaravakis@gmail.com>
2018-12-14 16:04:58 -0700

    Lecture 155 Transforming Data

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -15,14 +15,21 @@ class Blog extends Component {
     componentDidMount () {
         const posts = axios.get('http://jsonplaceholder.typicode.com/posts')
             .then(response => {
-                this.setState({posts: response.data});
+                const posts = response.data.slice(0, 4);
+                const updatedPosts = posts.map(post => {
+                    return {
+                        ...post,
+                        author: 'Max'
+                    }
+                })
+                this.setState({posts: updatedPosts});
                 // console.log(response);
             });
     }
     
     render () {
         const posts = this.state.posts.map( post => {
-            return <Post key={post.id} title={post.title}/>
+            return <Post key={post.id} title={post.title} author={post.author}/>
             }
         );
 
