e8256fd3c2543ea141f4c74756f5505780c9e3b7
Nick Karavakis <nkaravakis@gmail.com>
2018-12-18 14:18:32 -0700

    Lecture 201 Redirecting Requests

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
-import {Route, NavLink, Switch} from 'react-router-dom'
+import {Route, NavLink, Switch, Redirect} from 'react-router-dom'
 
 import './Blog.css';
 import Posts from './Posts/Posts'
@@ -33,7 +33,9 @@ class Blog extends Component {
                 <Route path="/" render={() => <h1>Home 2</h1>}/> */}                
                 <Switch>                                        
                     <Route path="/new-post" component={NewPost}/>                    
-                    <Route path="/posts" component={Posts}/>                
+                    <Route path="/posts" component={Posts}/>
+                    <Redirect from="/" to="/posts" />
+                    {/* <Route path="/" component={Posts}/> */}
                 </Switch>
             </div>
         );
